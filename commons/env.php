2144850b7b1a074e45c2c9b99854a6043e774354
long392005 <longf03092005@gmail.com>
2025-02-18 23:38:12 +0700

    Describe your changes

--- commons/env.php
+++ commons/env.php
@@ -2,8 +2,8 @@
 
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
-define('BASE_URL'   , 'http://localhost/base_du_an_3/');
-define('BASE_URL_ADMIN'       , 'http://localhost/base_du_an_3/admin/');
+define('BASE_URL'   , 'http://localhost/base_du_an_1/');
+define('BASE_URL_ADMIN'       , 'http://localhost/base_du_an_1/admin/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
